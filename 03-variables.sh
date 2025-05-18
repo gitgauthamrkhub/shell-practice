83af753346ff9a6143ba1aab836cc4c0f576ff22
gauthamrk <gauthamrkaws@gmail.com>
2025-05-18 20:03:05 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Rishi
+PERSON2=kushu
+
+echo "$PERSON1, Hey $PERSON2 How are you?"
+echo "$PERSON2, Hello $PERSON1, Iam fine How are you doing"
+echo "$PERSON1, Iam fine too. What's up?"
+echo "$PERSON2, Nothing just playing"
+echo "$PERSON1, Ok carry on bye"
