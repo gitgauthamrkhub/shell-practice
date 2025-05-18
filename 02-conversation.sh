63da292f53c8936de6f2d7e77c83c5e3c7d4fc65
gauthamrk <gauthamrkaws@gmail.com>
2025-05-18 19:40:33 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+
+#!bin/bash
+
+echo "Rishi, Hey Kushu How are you?"
+echo "Kushu, Hello Rishi, Iam fine How are you doing"
+echo "Rishi, Iam fine too. What's up?"
+echo "Kushu, Nothing just playing"
+echo "Rishi, Ok carry on bye"
